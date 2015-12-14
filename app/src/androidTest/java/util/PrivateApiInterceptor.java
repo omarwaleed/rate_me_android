ae4fbea37f14109dd698a7cea8afabb780bef60c
omarwaleed <omarpersonalprofile@gmail.com>
2015-12-14 21:37:36 +0200

    beginning integration

---
+++ app/src/androidTest/java/util/PrivateApiInterceptor.java
@@ -0,0 +1,16 @@
+package util;
+
+import retrofit.RequestInterceptor;
+
+public class PrivateApiInterceptor implements RequestInterceptor {
+	private String token;
+
+	public PrivateApiInterceptor(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public void intercept(RequestFacade request) {
+		request.addHeader("Authorization", token);
+	}
+}
---
+++ app/src/androidTest/java/util/PrivateApiRoutes.java
@@ -0,0 +1,13 @@
+package util;
+
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.PATCH;
+import retrofit.http.Path;
+
+public interface PrivateApiRoutes {
+
+    @PATCH("/products/{product_id}/buy")
+    void patchProductBuy(@Path("product_id") long productId, Callback<Response> callback);
+}
